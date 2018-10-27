e237d13763971cea3c62e4afe06d60c9f649f64b
mnuzen <43756178+mnuzen@users.noreply.github.com>
2018-10-27 14:24:24 -0700

    Add files via upload

--- lab3c.py
+++ lab3c.py
@@ -47,12 +47,12 @@ def update(lsys,l_str):
     the string with corresponding strings in the dictionary."""
     keys = list(lsys.keys())
     main_string = l_str
-    f_str=""
+    f_str = ""
     for char in main_string:
         if char in keys:
-            f_str+=lsys[char]
+            f_str += lsys[char]
         else:
-            f_str+=char
+            f_str += char
     return f_str
 
 def iterate(lsys, n):
@@ -64,7 +64,8 @@ def iterate(lsys, n):
     return l_str
 
 def lsystemToDrawingCommands(draw, s):
-    """ """
+    """Takes in a dictionary of drawing commands and returns the commands
+    corresponding to the l-system string taken in."""
     keys = list(draw.keys())
     cmds = []
     for char in s:
@@ -73,16 +74,55 @@ def lsystemToDrawingCommands(draw, s):
     return cmds
 
 def bounds(cmds):
-    '''<docstring>'''
-    pass
+    """Takes in a list of commands and computes the maximum and minimum a turtle
+    can travel with that list of commands."""
+    xmin = float()
+    ymin = float()
+    xmax = float()
+    ymax = float()
+    x = 0
+    y = 0
+    angle = 0
+    for cmd in cmds:
+        z = nextLocation(x, y, angle, cmd)
+        (x, y, angle) = z
+        if x < xmin:
+            xmin = x
+        if x > xmax:
+            xmax = x
+        if y < ymin:
+            ymin = y
+        if y > ymax:
+            ymax = y     
+    return xmin, xmax, ymin, ymax
 
 def nextLocation(x, y, angle, cmd):
-    '''<docstring>'''
-    pass
+    """Takes in current x, y, angle of the turtle and a new drawing command.
+    Implements the drawing command returns new x, y, and angle."""
+    cmds = cmd.split()
+    angle = angle * (math.pi/180)
+    if "L" in cmds:
+        cmd_num = int(cmds[1]) * (math.pi/180)
+        angle = angle + cmd_num
+    elif "R" in cmds: 
+        cmd_num = int(cmds[1]) * (math.pi/180)
+        angle = angle - cmd_num
+    else:
+        x += math.cos(angle) * int(cmds[1])
+        y += math.sin(angle) * int(cmds[1])
+    return x, y, (angle / (math.pi/180)) % 360
 
 def saveDrawing(filename, bounds, cmds):
-    '''<docstring>'''
-    pass
+    """Takes in a file, bounds, and a list of commands. Writes the bounds and
+    commands onto separate into the file."""
+    file = open(filename, 'w')
+    bound = ""
+    for b in bounds:
+        bound += (str(b) + " ")
+    file.write(str(bound))
+    for cmd in cmds:
+        file.write("\n" + cmd)
+    file.close()
 
 def makeDrawings(name, lsys, ldraw, imin, imax):
     '''Make a series of L-system drawings.'''
