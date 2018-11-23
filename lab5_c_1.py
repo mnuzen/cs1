803b525495d22675e4c884b69274f02b95c38bb9
mnuzen <43756178+mnuzen@users.noreply.github.com>
2018-11-23 10:21:23 -0800

    Add files via upload

--- lab5_c_1.py
+++ lab5_c_1.py
@@ -1,15 +1,16 @@
 from tkinter import *
 import random
-
-# Graphics commands.
-# create global list
-
-def draw_circle(canvas, color, pos_tup):
-    ''' '''
-    # choose a random diameter
-    # draw a circle with global color?
-    # center = mouse click
-    # store circles in a list or something
+    
+def draw_circle(canvas, color, x, y):
+    '''Takes in a canvas, color, and a position in the form of a 
+    tuple and creates a circle on given canvas. Generates random radius.'''
+    global lst
+    r = random.randint(10, 50)
+    # create circle list
+    circ = canvas.create_circle(x, y, r, fill=color, \
+                                       outline=color)  
+    lst.append(circ)
+    return circ
     
 def random_color():
     '''Generates a random color value in the format #RRGGBB as a string.'''
@@ -18,41 +19,45 @@ def random_color():
         r_color += random.choice('0123456789abcdef')
     return r_color
 
-def random_size():
-    '''Takes in integers, one bigger than the other, and returns
-    another random  integer in between them.'''
-    r = random.randint(10, 50)
-    return r
-
 # Event handlers.
 
 def key_handler(event):
     '''Handle key presses.'''
-    if event == '<q>':
+    if event.keysym == '<q>':
         root.bind('<q>', quit)
         
-    if event == '<c>':
+    if event.keysym == '<c>':
         color = random_color()
        
-    if event == '<x>':
+    if event.keysym == '<x>':
         # clear list of circles
+        for circle in lst:
+            canvas.delete(circle)
 
 def button_handler(event):
     '''Handle left mouse button click events.'''
     # take in position of the mouse button click
-    k
+    global x0, y0
+    x0 = event.x
+    y0 = event.y
 
 if __name__ == '__main__':
+    global root
     root = Tk()
     root.geometry('800x800')
+    global canvas
     canvas = Canvas(root, width=800, height=800)
     canvas.pack()
-
-    # put in a random color
+        
+    # Generate initial random color
+    r = random_color()
 
     # Bind events to handlers.
     root.bind('<Key>', key_handler)
     canvas.bind('<Button-1>', button_handler)
+    
+    # Generate circles
+    draw_circle(canvas, r, x0, y0)
 
     # Start it up.
     root.mainloop()
