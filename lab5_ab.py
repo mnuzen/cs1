0138e90cefa82bb8b48494c82dd0e0c245f3f6f6
mnuzen <43756178+mnuzen@users.noreply.github.com>
2018-11-26 16:52:42 -0800

    Added lab 5 files via upload

--- lab5_ab.py
+++ lab5_ab.py
@@ -5,11 +5,13 @@ import math
 class Point:
     """Creates Point Objects that have three coordinates: x, y, z."""
     def __init__(self, x, y, z):
+        """Initializes x, y, z."""
         self.x = x
         self.y = y
         self.z = z
     
     def distanceTo(self, new_point):
+        """Returns distance between two points."""
         x = self.x
         y = self.y
         z = self.z
@@ -19,11 +21,13 @@ class Point:
 class Triangle:
     """Creates a triangle with three points."""
     def __init___(self, P1, P2, P3):
+        """Initializes triangle."""
         self.p1 = P1
         self.p2 = P2
         self.p3 = P3
         
     def area(self):
+        """Returns the area of a triangle."""
         p1 = self.p1
         p2 = self.p2
         p3 = self.p3        
@@ -39,24 +43,30 @@ class Triangle:
 class Averager:
     """Holds a list and returns averages, mins, maxs, etc."""
     def __init__(self):
+        """Initializes the averager."""
         self.nums = []
         self.total = sum(self.nums)
         self.n = len(self.nums)
         
     def getTotal(self):
+        """Returns total."""
         self.total = sum(self.nums)
         return self.total
         
     def getNums(self):
+        """Returns num"""
         return self.nums[:]
     
     def append(self, num):
+        """Adds to num"""
         self.nums.append(num)
     
     def extend(self, n_lst):
+        """Extends list of numbers"""
         self.nums.extend(n_lst)
         
     def average(self):
+        """Returns average"""
         self.total = sum(self.nums)
         self.n = len(self.nums)        
         try:
@@ -65,6 +75,7 @@ class Averager:
             return 0.0
     
     def limits(self):
+        """Returns limits"""
         self.n = len(self.nums)                
         if self.n != 0:
             max_n = max(self.nums)
